ff0f39d2b398c7cc5e4439fab542f7b8620c8106
Armen Gevorgyan <gevorgyan.a@n-t.io>
2022-01-26 19:03:07 +0400

    Test-2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,7 +1,13 @@
 public class MainClass {
 
+    private static int classNumber = 20;
+
     public static int getLocalNumber() {
         return 14;
+    }
+
+    public static int getClassNumber() {
+        return classNumber;
     }
 
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -8,4 +8,9 @@ public class MainClassTest {
         Assertions.assertEquals(14, MainClass.getLocalNumber(),
                 "getLocalNumber method returns incorrect result.");
     }
+
+    @Test
+    public void testGetClassNumber() {
+        Assertions.assertTrue(MainClass.getClassNumber()>45, "Returned number is not great, than 45");
+    }
 }
